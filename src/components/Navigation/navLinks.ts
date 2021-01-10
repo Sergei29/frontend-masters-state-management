69b28336f0ddb763953efef6b15a3065b1be0fdd
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-01-10 16:59:40 +0000

    navlinks updated

--- src/components/Navigation/navLinks.ts
+++ src/components/Navigation/navLinks.ts
@@ -1,5 +1,4 @@
 export const arrNavLinks = [
   { id: 1, name: "Homepage", path: "/", exact: true },
   { id: 2, name: "Todo List", path: "/todos", exact: false },
-  { id: 4, name: "New Todo", path: "/todo_new", exact: false },
 ];
