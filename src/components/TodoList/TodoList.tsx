87ba9ebc6fe41c3aaa1dd1a0a6809f021fadeafa
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-01-10 13:05:43 +0000

    fetch todo list actions created

--- src/components/TodoList/TodoList.tsx
+++ src/components/TodoList/TodoList.tsx
@@ -1,15 +1,43 @@
 import React from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
-import { Card, CardContent } from "@material-ui/core";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import { useTodoList } from "./useTodoList";
+import { TodoModel } from "../../models/todoModel/todoModel";
+//styles:
 import { style, ClassesType } from "./style";
 
 type Props = {
   classes: ClassesType;
 };
 const TodoList: React.FC<Props> = ({ classes }) => {
+  const { loading, data, error } = useTodoList();
+
+  if (loading)
+    return (
+      <Typography variant="h5" align="center">
+        Loading...
+      </Typography>
+    );
+
+  if (error) {
+    return (
+      <Typography variant="h5" align="center">
+        {error}
+      </Typography>
+    );
+  }
+
   return (
     <Card>
-      <CardContent className={classes.todoList}>List</CardContent>
+      <CardContent className={classes.todoList}>
+        {data &&
+          data.length &&
+          data.map((objTodo: InstanceType<typeof TodoModel>) => (
+            <Typography variant="h5" align="center" key={objTodo.id}>
+              {objTodo.title}
+            </Typography>
+          ))}
+      </CardContent>
     </Card>
   );
 };
---
+++ src/components/TodoList/useTodoList.ts
@@ -0,0 +1,17 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { fetchTodoList } from "../../redux/actions/todo/actions";
+import { RootStateType } from "../../redux/types/types";
+
+export const useTodoList = () => {
+  const dispatch = useDispatch();
+  const { loading, data, error } = useSelector(
+    (state: RootStateType) => state.todo
+  );
+
+  useEffect(() => {
+    dispatch(fetchTodoList());
+  }, [dispatch]);
+
+  return { loading, data: data as Record<string, any>, error };
+};
