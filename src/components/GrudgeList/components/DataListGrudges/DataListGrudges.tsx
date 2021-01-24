2efde85c8bae338794d205bcf6659d5d17aad268
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-01-24 14:56:06 +0000

    jsDOC added

--- src/components/GrudgeList/components/DataListGrudges/DataListGrudges.tsx
+++ src/components/GrudgeList/components/DataListGrudges/DataListGrudges.tsx
@@ -2,15 +2,27 @@ import React, { useContext, useState } from "react";
 import withStyles from "@material-ui/core/styles/withStyles";
 import { TextField, Card, CardContent } from "@material-ui/core";
 import { GrudgeContext } from "../../ContextProvider/ContextProvider";
+//styles:
 import { style, ClassesType } from "./style";
 
 type Props = {
   classes: ClassesType;
 };
+
+/**
+ * @description datalist component
+ * @param {Object} {classes MUI classes}
+ * @returns {JSX} markup
+ */
 const DataListGrudges: React.FC<Props> = ({ classes }): JSX.Element => {
   const [strSelectedGrudge, setStrSelectedGrudge] = useState<string>("");
   const { arrGrudges } = useContext(GrudgeContext);
 
+  /**
+   * @description input change handler
+   * @param {Object} objEvent input change event object
+   * @returns {undefined} sets local state
+   */
   const handleChange = (objEvent: React.ChangeEvent<HTMLInputElement>) =>
     setStrSelectedGrudge(() => objEvent.target.value);
 
