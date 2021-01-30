213c5eac03e312b8ec0b6686f3a5f8d01ef384fc
Sergetestcargo <protopulos.pulatikos@yandex.ru>
2021-01-30 14:45:32 +0000

    moved custom hook useFetch to hooks directory

--- src/components/StarWars/StarWars.tsx
+++ src/components/StarWars/StarWars.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Typography } from "@material-ui/core";
 import withStyles from "@material-ui/core/styles/withStyles";
-import useFetch from "./useFetch";
+import useFetch from "../../hooks/useFetch";
 //styles:
 import { style, ClassesType } from "./style";
 
